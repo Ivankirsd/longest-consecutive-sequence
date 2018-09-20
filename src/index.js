2689322b664558223eb14021ce5209f226bf8bfd
Ivankirsd <ivankirsd@gmail.com>
2018-09-20 15:46:40 +0300

    Upload result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let maxSequence = 0;
+
+  if (array.length < 2) {
+    return array.length;
+  }
+
+  array.sort((a,b) => {return a - b}).reduce((accumulator, currentValue, index, array) => {
+    if ( index === 0 ) {
+      return 1;
+    }
+
+    if ( currentValue === array[index - 1] ) {
+
+      return accumulator;
+    } else if (currentValue - 1 === array[index - 1]) {
+      accumulator++;
+
+      return accumulator;
+    } else {
+      if (accumulator >= maxSequence) {
+        maxSequence = accumulator;
+      }
+
+      return 1;
+    }
+  }, maxSequence);
+
+  return maxSequence;
 }
